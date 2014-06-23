e284af81660908183e167b93b00f5c49574a6763
arulalant <arulalant@gmail.com>
2014-06-24 02:01:06 +0530

    added background image uri/url as optional third arg

--- custom.js
+++ custom.js
@@ -14,18 +14,17 @@ requirejs.config({
 $([IPython.events]).on('app_initialized.NotebookApp', function(){
 
      require(['custom/livereveal/main'],function(livereveal){
-       // livereveal.parameters('theme', 'transition', 'fontsize', bg_img_path);
+       // livereveal.parameters('theme', 'transition', static_prefix);
        //   * theme can be: simple, sky, beige, serif, solarized
        //   (you will need aditional css for default, night, moon themes).
        //   * transition can be: linear, zoom, fade, none
-       //   (aditional transitions are cube, page, concave, default).
-       //   * fontsize is in % units, ie. you can choose 140% or 200%
        //livereveal.parameters('simple', 'zoom', '140%');
        //livereveal.parameters('sky', 'linear', '140%');
        //livereveal.parameters('beige', 'linear', '140%');
        //livereveal.parameters('serif', 'linear', '140%');
-       // fourth argument takes image url locally/Internet or empty string for no background image.
-       livereveal.parameters('serif', 'zoom', '140%', ''); //'https://raw.github.com/damianavila/par_IPy_slides_example/gh-pages/figs/star_wars_stormtroopers_darth_vader.jpg');
+       // third argument takes image url or uri for backgroud image or empty string for no-background image.
+       livereveal.parameters('simple', 'zoom', ''); //'https://raw.github.com/damianavila/par_IPy_slides_example/gh-pages/figs/star_wars_stormtroopers_darth_vader.jpg');
+       //livereveal.parameters('simple', 'zoom');
        console.log('Live reveal extension loaded correctly');
      });
 

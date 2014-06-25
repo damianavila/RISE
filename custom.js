6631b9aa206191a76f6e874f53aa031921cbbde9
arulalant <arulalant@gmail.com>
2014-06-25 16:41:34 +0530

    added background image uri/url optional arg

--- custom.js
+++ custom.js
@@ -18,7 +18,7 @@ $([IPython.events]).on('app_initialized.NotebookApp', function(){
        //   * theme can be: simple, sky, beige, serif, solarized
        //   (you will need aditional css for default, night, moon themes).
        //   * transition can be: linear, zoom, fade, none
-       livereveal.parameters('simple', 'zoom');
+       livereveal.parameters('simple', 'zoom', ''); //'https://raw.github.com/damianavila/par_IPy_slides_example/gh-pages/figs/star_wars_stormtroopers_darth_vader.jpg');
        console.log('Live reveal extension loaded correctly');
      });
 

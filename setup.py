6df4caddc1b3470f9f2d3a0b408fa5782ba3e79f
Maarten Breddels <maartenbreddels@gmail.com>
2015-12-23 14:55:46 +0100

    Making installation as user non-default

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from notebook.services.config import ConfigManager
 
 livereveal_dir = os.path.join(os.path.dirname(__file__), 'livereveal')
 
-def install(use_symlink=False, enable=True):
+def install(use_symlink=False, enable=True, user=False):
     # Install the livereveal code.
     install_nbextension(livereveal_dir, symlink=use_symlink,
-                        overwrite=use_symlink, user=True)
+                        overwrite=use_symlink, user=user)
 
     if enable:
         cm = ConfigManager()
@@ -25,13 +25,15 @@ def main():
     install_parser = subparsers.add_parser('install')
     install_parser.add_argument('--develop', action='store_true',
                                 help="Install livereveal  as a symlink to the source.")
+    install_parser.add_argument('--user', action='store_true',
+                                help="Install as user")
     install_parser.add_argument('--no-enable', action='store_true',
                                 help="Install but don't enable the extension.")
 
     args = parser.parse_args(sys.argv[1:])
 
     install(use_symlink=args.develop,
-            enable=(not args.no_enable))
+            enable=(not args.no_enable), user=args.user)
 
 
 if __name__ == '__main__':

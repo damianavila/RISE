08ea93ab4a3af7e06a009c1addcd9fc872667d88
Damian Avila <damianavila@gmail.com>
2018-06-01 15:12:50 -0700

    Remove unuseful comments and implememts a very basic markupSlides function only considering slides-type cells

--- jupyterlab/jupyterlab_rise/src/index.ts
+++ jupyterlab/jupyterlab_rise/src/index.ts
@@ -25,16 +25,6 @@ import {
   JSONObject
 } from '@phosphor/coreutils';
 
-//import {
-//  CodeMirrorEditor
-//}  from '@jupyterlab/codemirror';
-
-//import {
-//  Reveal
-//} from 'reveal.js/js/reveal.js';
-
-//import 'reveal.js/css/reveal.css';
-//import 'reveal.js/css/theme/simple.css';
 import '../style/index.css';
 
 /**
@@ -47,110 +37,72 @@ const extension: JupyterLabPlugin<void> = {
 };
 
 /**
- * A notebook widget extension that adds a button to the toolbar.
+ * A notebook widget extension that adds a RISE button to the toolbar.
  */
 export
-class ButtonExtension implements DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel> {
+class RiseExtension implements DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel> {
   /**
    * Create a new extension object.
    */
   createNew(panel: NotebookPanel, context: DocumentRegistry.IContext<INotebookModel>): IDisposable {
     let callback = () => {
-      //*** working
-      //let cells = panel.notebook.model.cells;
-      //let slideshow = (cells.get(0).metadata.get("slideshow") || {});
-      //let slide_type = (slideshow as JSONObject)['slide_type'];
-      //console.log(slide_type);
 
       function get_slide_type(cell: any) {
         let slideshow = (cell.metadata.get("slideshow") || {});
         let slide_type = (slideshow as JSONObject)['slide_type'];
-        console.log(slide_type);
+        //console.log(slide_type);
        return ((slide_type === undefined) || (slide_type == '-')) ? '' : slide_type;
       }
 
-      function is_slide(cell: any)    {console.log(get_slide_type(cell) == 'slide'); return get_slide_type(cell) == 'slide';}
+      // function is_slide(cell: any)    {return get_slide_type(cell) == 'slide';}
       // function is_subslide(cell: any) {return get_slide_type(cell) == 'subslide';}
       // function is_fragment(cell: any) {return get_slide_type(cell) == 'fragment';}
-      // function is_regular(cell: any)  {return get_slide_type(cell) == '';}
-  
-      let cells = panel.notebook.model.cells;
-      let cell1 = cells.get(0);
-      is_slide(cell1);
+      // function is_regular(cell: any)  {return get_slide_type(cell) == '';
 
-      //*** info
-      // let cell = cells.get(0);
-      //*** working
-      // let cell = panel.notebook.node.children[0];
-      // let cell_copy = cell.cloneNode(true);
-      // 
-      // let section = document.createElement('section');
-      // section.appendChild(cell_copy);
-      // 
-      // let notebook = panel.notebook.node;
-      // notebook.replaceChild(section, cell);
-      //*** working
-      // let cell = panel.notebook.node.children[0];
-      // let s_cell = "<section>" + cell.outerHTML;
-      // cell.outerHTML = s_cell;
-      //*** working partially (not executable)
-      // let cell_0 = panel.notebook.node.children[0];
-      // let cell_1 = panel.notebook.node.children[1];
-      // let cell_0_copy = cell_0.cloneNode(true);
-      // let cell_1_copy = cell_1.cloneNode(true);
-      // 
-      // let section = document.createElement('section');
-      // section.appendChild(cell_0_copy);
-      // section.appendChild(cell_1_copy); 
-      // let notebook = panel.notebook.node;
-      // notebook.removeChild(cell_1);
-      // notebook.replaceChild(section, cell_0);
-      //*** working !!!
-      let cell_0 = panel.notebook.node.children[0];
-      let cell_1 = panel.notebook.node.children[1];
-      let cell_2 = panel.notebook.node.children[2];
-      let cell_3 = panel.notebook.node.children[3];
-      
-      let section_1 = document.createElement('section');
-      section_1.appendChild(cell_0);
-      section_1.appendChild(cell_1); 
+      function markupSlides(container: any) {
+        let slide_section;
+        let slide_counter = 0;
+        let cells = container.model.cells;
 
-      let section_2 = document.createElement('section');
-      section_2.appendChild(cell_2);
-      section_2.appendChild(cell_3); 
-      
-      let notebook = panel.notebook.node;
-      notebook.insertBefore(section_2, notebook.lastChild);
-      notebook.insertBefore(section_1, section_2);
+        for (let i=0; i < cells.length; i++) {
+          let cell = cells.get(i);
+          let slide_type = get_slide_type(cell);
 
-      //*** working
+          let cell_node = container.node.children[slide_counter];
+          let prev_slide_section = slide_section;
+
+          if (slide_type === 'slide') {
+            // Start new slide
+            slide_section = document.createElement('section');
+            slide_section.appendChild(cell_node);
+            if (i === 0) {
+              container.node.insertBefore(slide_section, container.node.firstChild);
+            } else {
+              container.node.insertBefore(slide_section, prev_slide_section.nextSibling);
+            }
+            slide_counter++;
+            console.log("Sep slide", i);
+          } else {
+            slide_section.appendChild(cell_node);
+            console.log("Nop slide", i);
+          }
+        console.log(container.node);
+        }
+      }
+
+      let notebook = panel.notebook;
+      markupSlides(notebook);
+
       let panel_container = document.getElementsByClassName("jp-NotebookPanel")[0];
       panel_container.classList.add("reveal");
-      console.log(panel_container);  
+      //console.log("panel_container");
 
       let notebook_container = document.getElementsByClassName("jp-Notebook")[0];
       notebook_container.classList.add("slides");
-      console.log(notebook_container);
-      // display: none; in jp-Toolbar to get rid of the toolbar
-      // *** working
-      // var headID = document.getElementsByTagName('head')[0];
-      // 
-      // var link_reveal = document.createElement('link');
-      // link_reveal.type = 'text/css';
-      // link_reveal.rel = 'stylesheet';
-      // link_reveal.id = 'reveal_css';
-      // headID.appendChild(link_reveal);
-      // link_reveal.href = './node_modules/reveal.js/css/reveal.css';
-      // 
-      // var link_theme = document.createElement('link');
-      // link_theme.type = 'text/css';
-      // link_theme.rel = 'stylesheet';
-      // link_theme.id = 'reveal_css';
-      // headID.appendChild(link_theme);
-      // link_theme.href = './node_modules/reveal.js/css/theme/simple.css';      
-      //
+      //console.log("notebook_container");
 
       let revealCSS = require('reveal_rise/reveal.css');
+      console.log(revealCSS);
       //let revealTheme = require('reveal.js/css/theme/simple.css');
 
       Reveal.initialize({
@@ -164,9 +116,6 @@ class ButtonExtension implements DocumentRegistry.IWidgetExtension<NotebookPanel
         minScale: 1.0,
         maxScale: 1.0
 			});
-      //console.log(headMin);
-      console.log(revealCSS);
-      //console.log(revealTheme);
     };
 
     let button = new ToolbarButton({
@@ -189,7 +138,7 @@ class ButtonExtension implements DocumentRegistry.IWidgetExtension<NotebookPanel
  * Activate the extension.
  */
 function activate(app: JupyterLab) {
-  app.docRegistry.addWidgetExtension('Notebook', new ButtonExtension());
+  app.docRegistry.addWidgetExtension('Notebook', new RiseExtension());
   console.log('JupyterLab extension jupyterlab_rise is activated!');
 };
 

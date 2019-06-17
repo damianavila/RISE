e9fe083e18c7c80b1b70f3d5bc223b402c1bfcf1
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2019-06-17 11:11:44 +0200

    first step towards themes working again in rise with reveal-3.8 theme-patching code gets promoted to a plain helper script in patch-reveal/

---
+++ patch-reveal/patch-reveal-themes.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# trigger this from toplevel directory with
+# 
+# npm run patch-themes
+
+# 
+# when going to reveal-3.8.0 we found out that reveal's native themes 
+# like simple, sky, and similar, failed to have their global background 
+# show up in our slides
+# 
+# this script patches the original themes as shipping with reveal.js to
+# fix this issue
+# 
+# (*) one thing that goes wrong is when reveal tries to tweak settings 
+#     on just the 'body' tag; these don't make it to <body> because they 
+#     are too general and get superseded by other css in the jupyter arena
+#     so we replace these 'body {' definitions so they apply on
+#     a more specific css selector
+#
+# (*) that is still not enough, it took a while to find out, but 
+#     the body tag also needs background-attachment to be reset to fixed,
+#     somehow something defines it to 'scroll' which breaks it for those themes
+# 
+
+[ -d node_modules ] || {
+    echo "$0: need to npm install first"
+}
+
+for theme in node_modules/reveal.js/css/theme/*.css; do
+    # do those changes only once
+    # there's no mention of rise-enabled in the stock reveal.js
+    grep -q 'rise-enabled' $theme && { echo $theme already patched; continue; }
+    echo patching theme $theme for RISE
+    sed -i.patched \
+        -e "/^body/ a\\
+    /* PATCHED by $0 */\\
+    background-attachment: fixed !important;" \
+        -e "s|^body {|body.notebook_app.rise-enabled {|" \
+        $theme  
+done

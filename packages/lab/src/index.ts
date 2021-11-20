5203b1fa36441f170ded94bd81cceff40949e463
Frédéric Collonval <fcollonval@gmail.com>
2021-11-20 17:25:40 +0100

    Preview side-by-side in JLab

--- packages/lab/src/index.ts
+++ packages/lab/src/index.ts
@@ -1,61 +1,171 @@
 import {
+  ILayoutRestorer,
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
-import { CommandToolbarButton, ICommandPalette } from '@jupyterlab/apputils';
+import {
+  CommandToolbarButton,
+  ICommandPalette,
+  WidgetTracker
+} from '@jupyterlab/apputils';
 
 import { PageConfig } from '@jupyterlab/coreutils';
+import { DocumentRegistry } from '@jupyterlab/docregistry';
 
-import { INotebookTracker, NotebookPanel } from '@jupyterlab/notebook';
+import {
+  INotebookModel,
+  INotebookTracker,
+  NotebookPanel
+} from '@jupyterlab/notebook';
 
 import { ITranslator } from '@jupyterlab/translation';
 
+import { ReadonlyPartialJSONObject } from '@lumino/coreutils';
+
 import { RISEIcon } from './icons';
+
+import {
+  RisePreview,
+  IRisePreviewTracker,
+  RisePreviewFactory
+} from './preview';
+
+export { IRisePreviewTracker } from './preview';
 
 /**
  * Command IDs namespace for JupyterLab RISE extension
  */
 namespace CommandIDs {
+  /**
+   * Open the current notebook in a new browser tab
+   */
   export const openRise = 'rise-jupyterlab:open';
+  /**
+   * Open the current notebook in a IFrame within JupyterLab
+   */
+  export const risePreview = 'rise-jupyterlab:preview';
 }
 
 /**
  * Open the notebook with RISE.
  */
-const plugin: JupyterFrontEndPlugin<void> = {
+const plugin: JupyterFrontEndPlugin<IRisePreviewTracker> = {
   id: 'rise-jupyterlab:plugin',
   autoStart: true,
   requires: [ITranslator],
-  optional: [INotebookTracker, ICommandPalette],
+  optional: [INotebookTracker, ICommandPalette, ILayoutRestorer],
+  provides: IRisePreviewTracker,
   activate: (
     app: JupyterFrontEnd,
     translator: ITranslator,
     notebookTracker: INotebookTracker | null,
-    palette: ICommandPalette | null
-  ) => {
+    palette: ICommandPalette | null,
+    restorer: ILayoutRestorer | null
+  ): IRisePreviewTracker => {
+    // Create a widget tracker for Rise Previews.
+    const tracker = new WidgetTracker<RisePreview>({
+      namespace: 'rise'
+    });
+
     if (!notebookTracker) {
-      return;
+      return tracker;
     }
 
-    const { commands, shell } = app;
-    const baseUrl = PageConfig.getBaseUrl();
+    const { commands, docRegistry, shell } = app;
     const trans = translator.load('rise');
+
+    const factory = new RisePreviewFactory(getRiseUrl, commands, {
+      name: 'rise',
+      fileTypes: ['notebook'],
+      modelName: 'notebook'
+    });
+
+    if (restorer) {
+      restorer.restore(tracker, {
+        command: 'docmanager:open',
+        args: panel => ({
+          path: panel.context.path,
+          factory: factory.name
+        }),
+        name: panel => panel.context.path,
+        when: app.serviceManager.ready
+      });
+    }
+
+    docRegistry.addWidgetFactory(factory);
+
+    function getCurrent(args: ReadonlyPartialJSONObject): NotebookPanel | null {
+      const widget = notebookTracker?.currentWidget ?? null;
+      const activate = args['activate'] !== false;
+
+      if (activate && widget) {
+        shell.activateById(widget.id);
+      }
+
+      return widget;
+    }
+
+    function isEnabled(): boolean {
+      return (
+        notebookTracker?.currentWidget !== null &&
+        notebookTracker?.currentWidget === shell.currentWidget
+      );
+    }
+
+    function getRiseUrl(path: string): string {
+      const baseUrl = PageConfig.getBaseUrl();
+      return `${baseUrl}rise/${path}`;
+    }
+
+    factory.widgetCreated.connect((sender, widget) => {
+      // Notify the widget tracker if restore data needs to update.
+      widget.context.pathChanged.connect(() => {
+        void tracker.save(widget);
+      });
+      // Add the notebook panel to the tracker.
+      void tracker.add(widget);
+    });
 
     commands.addCommand(CommandIDs.openRise, {
       label: args => (args.toolbar ? '' : trans.__('Open as Reveal Slideshow')),
-      caption: trans.__('Open the current notebook as an RevealJS slideshow.'),
+      caption: trans.__(
+        'Open the current notebook in a new browser tab as an RevealJS slideshow.'
+      ),
       icon: RISEIcon,
-      execute: () => {
+      execute: async () => {
         const current = notebookTracker.currentWidget;
         if (!current) {
           return;
         }
-        window.open(`${baseUrl}rise/${current.context.path}`);
+        await current.context.save();
+        window.open(getRiseUrl(current.context.path));
       },
-      isEnabled: () =>
-        notebookTracker.currentWidget !== null &&
-        notebookTracker.currentWidget === shell.currentWidget
+      isEnabled
+    });
+
+    commands.addCommand(CommandIDs.risePreview, {
+      label: args =>
+        args.toolbar ? '' : trans.__('Render as Reveal Slideshow'),
+      caption: trans.__('Render the current notebook as Reveal Slideshow'),
+      icon: RISEIcon,
+      execute: async args => {
+        const current = getCurrent(args);
+        let context: DocumentRegistry.IContext<INotebookModel>;
+        if (current) {
+          context = current.context;
+          await context.save();
+
+          commands.execute('docmanager:open', {
+            path: context.path,
+            factory: 'rise',
+            options: {
+              mode: 'split-right'
+            }
+          });
+        }
+      },
+      isEnabled
     });
 
     notebookTracker.widgetAdded.connect(
@@ -65,7 +175,7 @@ const plugin: JupyterFrontEndPlugin<void> = {
           'RISE-button',
           new CommandToolbarButton({
             commands,
-            id: CommandIDs.openRise,
+            id: CommandIDs.risePreview,
             args: { toolbar: true }
           })
         );
@@ -73,8 +183,13 @@ const plugin: JupyterFrontEndPlugin<void> = {
     );
 
     if (palette) {
-      palette.addItem({ command: CommandIDs.openRise, category: 'Other' });
+      const category = 'Notebook Operations';
+      [CommandIDs.openRise, CommandIDs.risePreview].forEach(command => {
+        palette.addItem({ command, category });
+      });
     }
+
+    return tracker;
   }
 };
 
---
+++ packages/lab/src/preview.ts
@@ -0,0 +1,290 @@
+import {
+  IFrame,
+  ToolbarButton,
+  IWidgetTracker,
+  Toolbar
+} from '@jupyterlab/apputils';
+
+import {
+  ABCWidgetFactory,
+  DocumentRegistry,
+  DocumentWidget
+} from '@jupyterlab/docregistry';
+
+import { INotebookModel } from '@jupyterlab/notebook';
+
+import { refreshIcon } from '@jupyterlab/ui-components';
+
+import { CommandRegistry } from '@lumino/commands';
+
+import { Token } from '@lumino/coreutils';
+
+import { Message } from '@lumino/messaging';
+
+import { Signal } from '@lumino/signaling';
+
+import { Widget } from '@lumino/widgets';
+
+import { RISEIcon } from './icons';
+
+/**
+ * A class that tracks Rise Preview widgets.
+ */
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface IRisePreviewTracker extends IWidgetTracker<RisePreview> {}
+
+/**
+ * The Rise Preview tracker token.
+ */
+export const IRisePreviewTracker = new Token<IRisePreviewTracker>(
+  'rise-jupyterlab:IRisePreviewTracker'
+);
+
+/**
+ * A DocumentWidget that shows a Rise preview in an IFrame.
+ */
+export class RisePreview extends DocumentWidget<IFrame, INotebookModel> {
+  /**
+   * Instantiate a new RisePreview.
+   * @param options The RisePreview instantiation options.
+   */
+  constructor(options: RisePreview.IOptions) {
+    super({
+      ...options,
+      content: new IFrame({
+        sandbox: [
+          'allow-same-origin',
+          'allow-scripts',
+          'allow-downloads',
+          'allow-modals',
+          'allow-popups'
+        ]
+      })
+    });
+
+    window.onmessage = (event: any) => {
+      switch (event.data?.level) {
+        case 'debug':
+          console.debug(...event.data?.msg);
+          break;
+
+        case 'info':
+          console.info(...event.data?.msg);
+          break;
+
+        case 'warn':
+          console.warn(...event.data?.msg);
+          break;
+
+        case 'error':
+          console.error(...event.data?.msg);
+          break;
+
+        default:
+          console.log(event);
+          break;
+      }
+    };
+
+    const { getRiseUrl, context, renderOnSave } = options;
+
+    this.content.url = getRiseUrl(context.path);
+    this.content.title.icon = RISEIcon;
+
+    this._renderOnSave = renderOnSave ?? false;
+
+    context.pathChanged.connect(() => {
+      this.content.url = getRiseUrl(context.path);
+    });
+
+    const reloadButton = new ToolbarButton({
+      icon: refreshIcon,
+      tooltip: 'Reload Preview',
+      onClick: () => {
+        this.reload();
+      }
+    });
+
+    const renderOnSaveCheckbox = new Private.CheckBox({
+      checked: this._renderOnSave,
+      onChange: (event: Event) => {
+        this._renderOnSave = (event.target as any)?.checked ?? false;
+      }
+    });
+
+    this.toolbar.addItem(
+      'open',
+      new ToolbarButton({
+        icon: RISEIcon,
+        tooltip: 'Open in a new browser tab',
+        onClick: () => {
+          options.commands.execute('rise-jupyterlab:open');
+        }
+      })
+    );
+
+    if (context) {
+      this.toolbar.addItem('renderOnSave', renderOnSaveCheckbox);
+      void context.ready.then(() => {
+        context.fileChanged.connect(() => {
+          if (this.renderOnSave) {
+            this.reload();
+          }
+        });
+      });
+    }
+
+    this.toolbar.addItem('spacer', Toolbar.createSpacerItem());
+
+    this.toolbar.addItem('reload', reloadButton);
+  }
+
+  /**
+   * Dispose the preview widget.
+   */
+  dispose(): void {
+    if (this.isDisposed) {
+      return;
+    }
+    super.dispose();
+    Signal.clearData(this);
+  }
+
+  /**
+   * Reload the preview.
+   */
+  reload(): void {
+    const iframe = this.content.node.querySelector('iframe')!;
+    if (iframe.contentWindow) {
+      iframe.contentWindow.location.reload();
+    }
+  }
+
+  /**
+   * Whether the preview reloads when the context is saved.
+   */
+  get renderOnSave(): boolean {
+    return this._renderOnSave;
+  }
+
+  private _renderOnSave: boolean;
+}
+
+/**
+ * A namespace for RisePreview statics.
+ */
+export namespace RisePreview {
+  /**
+   * Instantiation options for `RisePreview`.
+   */
+  export interface IOptions
+    extends DocumentWidget.IOptionsOptionalContent<IFrame, INotebookModel> {
+    /**
+     * Application commands registry
+     */
+    commands: CommandRegistry;
+    /**
+     * The Rise URL function.
+     */
+    getRiseUrl: (path: string) => string;
+
+    /**
+     * Whether to reload the preview on context saved.
+     */
+    renderOnSave?: boolean;
+  }
+}
+
+export class RisePreviewFactory extends ABCWidgetFactory<
+  RisePreview,
+  INotebookModel
+> {
+  defaultRenderOnSave = false;
+
+  constructor(
+    private getRiseUrl: (path: string) => string,
+    private commands: CommandRegistry,
+    options: DocumentRegistry.IWidgetFactoryOptions<RisePreview>
+  ) {
+    super(options);
+  }
+
+  protected createNewWidget(
+    context: DocumentRegistry.IContext<INotebookModel>
+  ): RisePreview {
+    return new RisePreview({
+      context,
+      commands: this.commands,
+      getRiseUrl: this.getRiseUrl,
+      renderOnSave: this.defaultRenderOnSave
+    });
+  }
+}
+
+namespace Private {
+  /**
+   * Namespace for the checkbox widget
+   */
+  export namespace CheckBox {
+    /**
+     * Constructor options for the checkbox
+     */
+    export interface IOptions {
+      /**
+       * Checkbox initial value
+       */
+      checked?: boolean;
+      /**
+       * Callback on checked status changes
+       */
+      onChange?: (ev: Event) => void;
+    }
+  }
+
+  /**
+   * Simple checkbox
+   */
+  export class CheckBox extends Widget {
+    constructor(options: CheckBox.IOptions = {}) {
+      const node = document.createElement('label');
+      node.insertAdjacentHTML(
+        'afterbegin',
+        '<input name="renderOnSave" type="checkbox"></input>Render on Save'
+      );
+      super({ node });
+      this.input = node.childNodes.item(0) as HTMLInputElement;
+      this.checked = options.checked ?? false;
+      const noOp = () => {
+        // no-op
+      };
+      this.onChange = options.onChange ?? noOp;
+    }
+
+    /**
+     * Checkbox status
+     */
+    get checked(): boolean {
+      return this.input.checked;
+    }
+    set checked(v: boolean) {
+      this.input.checked = v;
+    }
+
+    /**
+     * Checkbox status callback
+     */
+    protected onChange: (event: Event) => void;
+
+    protected onAfterAttach(msg: Message): void {
+      super.onAfterAttach(msg);
+      this.input.addEventListener('change', this.onChange);
+    }
+
+    protected onBeforeDetach(msg: Message): void {
+      this.input.removeEventListener('change', this.onChange);
+      super.onBeforeDetach(msg);
+    }
+
+    protected input: HTMLInputElement;
+  }
+}

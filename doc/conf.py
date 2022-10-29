ec2f8f3cfde89c35c78b9d68d410fcc624a9b48c
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2022-10-29 11:09:06 +0200

    tweaks in .readthedocs.yaml

--- doc/conf.py
+++ doc/conf.py
@@ -38,7 +38,7 @@ from rise import (
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.githubpages']
+extensions = ['myst_parser', 'sphinx.ext.githubpages']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -78,7 +78,7 @@ html_title = "RISE " + release
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
